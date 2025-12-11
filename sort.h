c86481818d600fb64415b0bd031116e00bb8b8a5
Julien <julien.hinlang@holbertonstudents.com>
2025-12-11 15:12:12 +0100

    feat: creating an algorithm to sort with insertion process

--- sort.h
+++ sort.h
@@ -11,6 +11,9 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
+
 void bubble_sort(int *array, size_t size);
 void print_array(int *array, size_t size);
 #endif
