9024d7c3132402baaecd609422cb3eaf38f807ac
Julien <julien.hinlang@holbertonstudents.com>
2025-12-11 14:27:54 +0100

    feat: correcting syntax to answer betty's rules

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,17 +1,26 @@
 #include "sort.h"
 /**
- * @tab: is the array to travel
- * @n: is the number of variables in the array
+ * @array: is the array to travel
+ * @size: is the number of variables in the array
  * 
  * Return: nothing
  */
 
 void bubble_sort(int *array, size_t size)
 {
-	int i, j, tmp;
+	size_t i, j;
+	int tmp;
+	int swapped;
+
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
+
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -19,7 +28,16 @@ void bubble_sort(int *array, size_t size)
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
+
+				swapped = 1;
+
+				print_array(array, size);
 			}
+		}
+
+		if (!swapped)
+		{
+			break;
 		}
 	}
 }
